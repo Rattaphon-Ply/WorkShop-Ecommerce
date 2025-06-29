cd67b790ce3304dfa0d7941a230a775499f8e349
Rattaphon-Ply <rattaphonsiri@gmail.com>
2025-06-30 02:27:27 +0700

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div>
       <ContentCarousel />
 
-      <p className="text-2xl text-center my-4 text-white">สินค้าขายดี</p>
+      <p className="text-2xl text-center my-4 text-white">สินค้าขายดี งงดิดิ</p>
       <BestSeller />
 
       <p className="text-2xl text-center my-4 text-white">สินค้าใหม่</p>
